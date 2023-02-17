b01641ee74bdb8027ed87b0ea125b7a8d9238dcb
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-02-17 08:57:48 -0500

    store

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var shop = [
     {
-        title: '',
-        image: '',
-        price: '',
+        title: 'hello',
+        image: 'hello',
+        price: 'ello',
     }
 ]
 var postHTML = " "
   
-  
-  
+ 
+
   
   
   
@@ -35,12 +35,13 @@ var postHTML = " "
   
   
   
-for (var i=0; i < shop.length ; i++){
-    var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
-    var image = '<img src="' + shop[i] + '"/'
-    var price = '<p> $' + .price + '</p></span>'
-    var description = '<div class=""><p>'+ .description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+for (var i = 0; i < shop.length; i++) {
+    var heading = '<div class="product"><span><h5>' + shop[i].title + '</h5></span>';
+    var image = '<img src="' + shop[i].image + '"/>';
+    var price = '<p>$' + shop[i].price + '</p></span>';
+    var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>';
     var concatThis = heading + image + price + description;
-    postHTML = postHTML + concatThis
-}
-document.getElementById('market').innerHTML = postHTML
+    postHTML = postHTML + concatThis;
+  }
+  
+  document.getElementById('market').innerHTML = postHTML;
